da2a020c8eef49ad4aa9b0bee4a72260a98d42d5
guharoytamajit <guharoytamajit@gmail.com>
2019-08-11 22:13:12 +0530

    minor change

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 	@GetMapping("/")
 public String hello() {
-	return "hello world";
+	return "hello world!!";
 }
 }
